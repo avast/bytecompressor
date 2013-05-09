750050f40a8706ff7b678198bc1b47f3b6ce00e5
Lukas Karas <lukas.karas@centrum.cz>
2013-05-09 18:48:08 +0200

    initial commit: code forked from jsnappy project, Avast Bytecompressor implementation and project files

---
+++ Readme.markdown
@@ -0,0 +1,27 @@
+Description
+-----------
+
+Java impmenentation of compression algorithm Snappy from Google. 
+This is a Avast a.s. fork of http://code.google.com/p/jsnappy/ , 
+it adds bytecompressor layer compatibility.
+
+
+License
+-------
+
+(Apache 2.0 License)
+
+Copyright 2011 Tor-Einar Jarnbjo
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
---
+++ src/main/scala/com/avast/jsnappy/SnappyCompressor.scala
@@ -0,0 +1,69 @@
+/*
+ *  Copyright 2013 Lukas Karas, Avast a.s. <karas@avast.com>
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package com.avast.jsnappy
+
+import com.avast.{ByteBufferBackedOutputStream, ByteBufferBackedInputStream, Compressor}
+import java.nio.ByteBuffer
+import de.jarnbjo.jsnappy._
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: karry
+ * Date: 6.5.13
+ * Time: 21:15
+ */
+object SnappyCompressor extends SnappyCompressor(
+  de.jarnbjo.jsnappy.SnappyCompressor.DEFAULT_EFFORT,
+  SnzOutputStream.DEFAULT_BUFFER_SIZE
+  ){
+
+  val DEFAULT_EFFORT = de.jarnbjo.jsnappy.SnappyCompressor.DEFAULT_EFFORT
+  val DEFAULT_BUFFER_SIZE = SnzOutputStream.DEFAULT_BUFFER_SIZE
+}
+
+/**
+ *
+ * @param effort 	 Sets the compression effort used by this stream from 1 (fastest, less
+ * compression) to 100 (slowest, best compression). If the effort is
+ * changed after the stream has been written to, the new effort will take
+ * effect on the next packet processed internally and may so affect
+ * data already written to the stream.
+ *
+ * @param bufferSize buffer size must be a power of 2 between 2**0 and 2**29
+ */
+class SnappyCompressor(val effort:Int, val bufferSize:Int) extends Compressor{
+
+  def decompress(compressedIn: ByteBuffer): ByteBuffer = {
+    val sis: SnzInputStream = new SnzInputStream(new ByteBufferBackedInputStream(compressedIn));
+    val out = new ByteBufferBackedOutputStream(math.max(1024, compressedIn.remaining() * 2))
+    pipe(sis, out);
+    sis.close();
+
+    out.asByteBuffer()
+  }
+
+  def compress(rawIn: ByteBuffer): ByteBuffer = {
+    val out = new ByteBufferBackedOutputStream(math.max(1024, rawIn.remaining() * 2))
+    val cos = new SnzOutputStream(out, bufferSize)
+    cos.setCompressionEffort(effort)
+    
+    pipe(new ByteBufferBackedInputStream(rawIn), cos);
+    cos.close()
+
+    out.asByteBuffer()
+  }
+}
