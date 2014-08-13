aa1c23871ee694151ce9edce8a32108f8f3e6431
Lukas Karas <karas@avast.com>
2014-08-13 19:12:14 +0200

    try to fix release process

--- signAndDeployResolvedPoms.sh
+++ signAndDeployResolvedPoms.sh
@@ -5,7 +5,16 @@
 # export gpgPass=*****
 # export scalaVersion=2.9
 
-for module in `ls -d1 . bytecompressor bytecompressor-huffman bytecompressor-jsnappy bytecompressor-zlib` ; do 
+mvn gpg:sign-and-deploy-file \
+	-P scala-${scalaVersion} \
+	-Durl=https://oss.sonatype.org/service/local/staging/deploy/maven2/ \
+	-DrepositoryId=sonatype-nexus-staging \
+	-Dgpg.keyname=${gpgKeyname} \
+	-DpomFile=./target/pom-property-fix/pom.xml \
+	-Dfile=./target/pom-property-fix/pom.xml \
+	-Dgpg.passphrase=${gpgPass}   
+
+for module in `ls -d1 bytecompressor bytecompressor-huffman bytecompressor-jsnappy bytecompressor-zlib` ; do 
 
 	mvn gpg:sign-and-deploy-file \
 		-P scala-${scalaVersion} \
@@ -13,7 +22,7 @@ for module in `ls -d1 . bytecompressor bytecompressor-huffman bytecompressor-jsn
 		-DrepositoryId=sonatype-nexus-staging \
 		-Dgpg.keyname=${gpgKeyname} \
 		-DpomFile=$module/target/pom-property-fix/pom.xml \
-		-Dfile=$module/target/pom-property-fix/pom.xml \
+		-Dfile=$module/${module}_${scalaVersion}-${releaseVersion}.jar \
 		-Dgpg.passphrase=${gpgPass}   
 
 done
